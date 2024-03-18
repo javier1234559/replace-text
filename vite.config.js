6989439364f81d3134aa657fb222e528415030c5
nhat <nhat1234559@gmail.com>
2024-03-19 03:29:19 +0700

    fix(deploy): wrong name dir

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/text-replace/',
+  base: '/replace-text/',
   plugins: [react()],
 })
 
